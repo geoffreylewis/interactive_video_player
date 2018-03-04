2d97a19be8544156ad40a7a4c6a277f10f819bd4
Geoff Lewis <geoff@geofflewis.work>
2018-03-04 15:22:10 -0700

    Added click event to transcript

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,13 @@
+const transWrap = document.getElementById('transcription');
 const transcripts = document.getElementById('transcription').getElementsByTagName('span');
 const video = document.querySelector('video');
+
+transWrap.addEventListener('click', (e) => {
+     if (e.target.tagName === 'SPAN') {
+          let startPoint = e.target.getAttribute('data-start');
+          video.currentTime = startPoint;
+     }
+})
 
 video.addEventListener('timeupdate', () => {
      for (let i = 0; i < transcripts.length; i += 1) {
